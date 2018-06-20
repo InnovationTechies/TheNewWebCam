532a6600c274b5bf6ec73e0c3650b4bde8fe5e03
Cwenga Zozo <cwengazozo@outlook.com>
2018-06-20 17:01:12 +0200

    Button Capture MVC

--- TheWebCamMvc/Controllers/HomeController.cs
+++ TheWebCamMvc/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace TheWebCamMvc.Controllers
 {
     public class HomeController : Controller
@@ -26,5 +27,51 @@ namespace TheWebCamMvc.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Capture()
+        {
+            try
+            {
+                if (img.Image != null)
+                {
+                    //Save First
+                    Picture pic = new Picture();
+
+                    Bitmap varBmp = new Bitmap(pictureBoxWeb.Image);
+                    Bitmap newBitmap = new Bitmap(varBmp);
+                    // varBmp.Save(@"C:\Users\s2132\source\repos\WebCamStreaming\WebCamStreaming\Pictures\" + DateTime.Now.ToString() + ".png", ImageFormat.Png);
+                    byte[] data;
+                    using (System.IO.MemoryStream stream = new System.IO.MemoryStream())
+                    {
+                        varBmp.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                        data = stream.ToArray();
+                    }
+                    pic.Image = data;
+                    pic.Name = "MyPicture" + DateTime.Now.ToString("yyyymmdd") + '_' + DateTime.Now.ToString("hhmmss");
+
+                    if (pic.SavePicture())
+                    {
+                        MessageBox.Show("Saved");
+                    }
+
+
+                    //Now Dispose to free the memory
+                    varBmp.Dispose();
+                    varBmp = null;
+
+                    MessageBox.Show("Picture is saved!", "Saved!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                { MessageBox.Show("null exception"); }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return View();
+        }
+
     }
 }
---
+++ TheWebCamMvc/Models/Picture.cs
@@ -0,0 +1,13 @@
+﻿namespace TheWebCamMvc
+{
+    public class Picture
+    {
+
+        public byte[] Image { get;  set; }
+        public string Name { get;  set; }
+
+
+
+
+    }
+}
